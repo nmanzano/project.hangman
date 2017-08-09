26378ce0bf015d233369eba267c96737e70c4273
Hack Reactor Student <student@hackreactor.com>
2017-08-09 16:55:28 -0500

    made some more improvements to hangman

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
+
 # Hangman
 One player thinks of a word, phrase or sentence and the other tries to guess it by suggesting letters or numbers, within a certain number of guesses.
 
--- index.js
+++ index.js
@@ -4,49 +4,11 @@
 var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
        'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
        't', 'u', 'v', 'w', 'x', 'y', 'z'];
-   canvas =  function(){
+var lives = 10;
 
-    myStickman = document.getElementById("stickman");
-    context = myStickman.getContext('2d');
-    context.beginPath();
-    context.strokeStyle = "#fff";
-    context.lineWidth = 2;
-      };
-    draw = function($pathFromx, $pathFromy, $pathTox, $pathToy) {
-    context.moveTo($pathFromx, $pathFromy);
-    context.lineTo($pathTox, $pathToy);
-    context.stroke();
+function loselives(){
+  lives +-;
 }
-   head = function(){
-      myStickman = document.getElementById("stickman");
-      context = myStickman.getContext('2d');
-      context.beginPath();
-      context.arc(60, 25, 10, 0, Math.PI*2, true);
-      context.stroke();
-          }
-  draw (0, 150, 150, 150);
-  draw (10, 0, 10, 600);
-  draw (0, 5, 70, 5);
-  draw (60, 5, 60, 15);
-
-  torso = function() {
-   draw (60, 36, 60, 70);
- };
-
- rightArm = function() {
-   draw (60, 46, 100, 50);
- };
-
- leftArm = function() {
-   draw (60, 46, 20, 50);
- };
-
- rightLeg = function() {
-   draw (60, 70, 100, 100);
- };
-
- leftLeg = function() {
-   draw (60, 70, 20, 100);
- };
-
-drawArray = [rightLeg, leftLeg, rightArm, leftArm,  torso,  head, frame4, frame3, frame2, frame1];
+if(lives == 0){
+  alert("Game Over");
+}
