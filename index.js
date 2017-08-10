5537801d495511902411f2f47ad3a612b3e1920d
Hack Reactor Student <student@hackreactor.com>
2017-08-10 12:28:41 -0500

    made the hangman site look neater

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
+$(document).ready(function() {
 
-
-
+var word = ['a', 'b', 'c'];
 var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
        'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
        't', 'u', 'v', 'w', 'x', 'y', 'z'];
 var lives = 10;
 
 function loselives(){
-  lives +-;
+  lives - 1;
 }
 if(lives == 0){
   alert("Game Over");
 }
+
+$( "#amountlives" ).append("Lives: " + lives);
+
+
+});
